76bc3471f24ff7d2910125bc5ae5f41c0b637bc2
NathanKloer <42096514+NathanKloer@users.noreply.github.com>
2019-02-11 09:53:03 -0500

    Update precache-manifest.9abf78caa05f7e737b63250bff639c79.js

--- precache-manifest.9abf78caa05f7e737b63250bff639c79.js
+++ precache-manifest.9abf78caa05f7e737b63250bff639c79.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+    "url": "/MyPortfolio/static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "cde22863ffca018955eb",
-    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+    "url": "/MyPortfolio/static/js/runtime~main.cde22863.js"
   },
   {
     "revision": "b951633cf80319f66888",
-    "url": "/react-gh-pages/static/js/main.b951633c.chunk.js"
+    "url": "/MyPortfolio/static/js/main.b951633c.chunk.js"
   },
   {
     "revision": "d16a266844ec01aafc8a",
-    "url": "/react-gh-pages/static/js/2.d16a2668.chunk.js"
+    "url": "/MyPortfolio/static/js/2.d16a2668.chunk.js"
   },
   {
     "revision": "b951633cf80319f66888",
-    "url": "/react-gh-pages/static/css/main.1e974121.chunk.css"
+    "url": "/MyPortfolio/static/css/main.1e974121.chunk.css"
   },
   {
     "revision": "009c5765ad8c0c24e2ff0ca3ab1efde3",
-    "url": "/react-gh-pages/index.html"
+    "url": "/MyPortfolio/index.html"
   }
 ];
